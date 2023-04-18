d2c182334b75fa4d18fd6246a683cb9da1953ba1
Enoch Klufio <Enochmk94@gmail.com>
2023-04-18 11:30:13 +0000

    refactor: update delete service

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,14 @@ import createNumber from './routes/createNumber.route';
 import integrationEnquiry from './routes/integrationEnquiry.route';
 import poolActivation from './routes/poolActivation.route';
 import download from './routes/download.route';
+import deleteNumber from './routes/deleteNumber.route';
 
 const router = Router();
 
 router.use(createNumber);
 router.use(integrationEnquiry);
 router.use(poolActivation);
+router.use(deleteNumber);
 router.use('/tools', poolActivation);
 router.use(download);
 
